e8cd9ff78c39c8abab45cd82f9ca450ab92a0961
A_F_Black <andras.fekete@capture.eu>
2020-04-26 21:46:04 +0200

    swspi

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2189,7 +2189,7 @@
    * The default SW SPI pins are defined the respective pins files,
    * but you can override or define them here.
    */
-  //#define TMC_USE_SW_SPI
+  #define TMC_USE_SW_SPI
   //#define TMC_SW_MOSI       -1
   //#define TMC_SW_MISO       -1
   //#define TMC_SW_SCK        -1
@@ -2328,10 +2328,10 @@
 
   #if EITHER(SENSORLESS_HOMING, SENSORLESS_PROBING)
     // TMC2209: 0...255. TMC2130: -64...63
-    #define X_STALL_SENSITIVITY  0
+    #define X_STALL_SENSITIVITY  -50
     #define X2_STALL_SENSITIVITY X_STALL_SENSITIVITY
-    #define Y_STALL_SENSITIVITY  0
-    #define Z_STALL_SENSITIVITY  0
+    #define Y_STALL_SENSITIVITY  -50
+    #define Z_STALL_SENSITIVITY  -50
     //#define SPI_ENDSTOPS              // TMC2130 only
     #define IMPROVE_HOMING_RELIABILITY
   #endif
